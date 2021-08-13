3d825b0c58c6156ed6c81ff5327efaabe17ddfe5
Chris <34993144+Mesiow@users.noreply.github.com>
2021-08-13 14:50:54 -0400

    finish main styling/clear form after submission

--- app.js
+++ app.js
@@ -7,17 +7,22 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 const api_url = `https://geo.ipify.org/api/v1?apiKey=${env.IPIFY_KEY}&ipAddress=8.8.8.8`;
-
+var json_data = null;
 
 app.get("/", (req, res) => {
    res.render("index"); 
 });
 
+app.post("/", (req, res) => {
+     //fetch data from api
+     console.log("post");
+     fetch(api_url)
+     .then(res => res.json())
+     .then(json => console.log(json));
+});
+
 app.get("/result", (req, res) => {
-    //fetch data from api
-    fetch(api_url)
-    .then(res => res.json())
-    .then(json => console.log(json));
+   
 
     //render data on ejs template
     //res.render("show", {data: json});
