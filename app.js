fafb4534f42cd8a2a40fd3af76cfb4f8c9e0ea53
Chris <34993144+Mesiow@users.noreply.github.com>
2021-08-14 16:59:00 -0400

    retrieve info from api and render it

--- app.js
+++ app.js
@@ -6,26 +6,20 @@ const env = require("./config");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-const api_url = `https://geo.ipify.org/api/v1?apiKey=${env.IPIFY_KEY}&ipAddress=8.8.8.8`;
-var json_data = null;
+const api_url = `https://geo.ipify.org/api/v1?apiKey=${env.IPIFY_KEY}`;
 
 app.get("/", (req, res) => {
-   res.render("index"); 
-});
-
-app.post("/", (req, res) => {
-     //fetch data from api
-     console.log("post");
-     fetch(api_url)
-     .then(res => res.json())
-     .then(json => console.log(json));
+   res.render("index", {data: null}); 
 });
 
 app.get("/result", (req, res) => {
-   
-
-    //render data on ejs template
-    //res.render("show", {data: json});
+    let domain = req.query.domain;
+    let url = api_url + `&domain=${domain}`;
+    fetch(url)
+     .then(res => res.json())
+     .then(json => {
+         res.render("index", {data: json});
+     });
 });
 
 const PORT = process.env.PORT || 3000;
